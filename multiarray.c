7419033a329aabac6c079cbf1531df4207a43234
Nakrit Aree <nakritaree@gmail.com>
2024-03-31 15:50:50 +0700

    Update multiarray

--- multiarray.c
+++ multiarray.c
@@ -2,8 +2,7 @@
 
 int main() {
     char word[5][12] = {"Hello", "World", "C", "Programming", "Language"};
-    for (int i = 0; i < 5; i++)
-        printf("%s\n", word[i]);
-    
+    printf("%c",word[4][3]);
     return 0;
 }
+
