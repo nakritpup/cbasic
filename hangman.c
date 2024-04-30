0645d790415e7d8d16be144cf725c2ee33d16789
Nakrit Aree <nakritaree@gmail.com>
2024-04-30 14:00:49 +0700

    hangman

--- hangman.c
+++ hangman.c
@@ -2,83 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 int main() {
-    char word[100];
-    char curr[100];
-    int wrong = 0;
+    char word[100][100] = {"thailand", "england", "india", "yellow", "green", "freeze", "breeze", "crack", "black", "myanmar", "superman", "ghostrider", "hero", "red", "eyes", "perfect"};
     int wordLength, i;
+    int wrong = 0;
     int gameWon = 0;
-    int hintGiven = 0;
-    system("cls");
 
-    printf("Enter the word to be guessed (in lowercase): ");
-    scanf("%s", word);
-    printf("--------------------------------------------------- \n");
+    srand(time(NULL));
+    int r = rand() % 16;
+    wordLength = strlen(word[r]);
 
-    for (i = 0; i < strlen(word); i++) {
-        if (!islower(word[i])) {
-            printf("--------------------------------------------------- \n");
-            printf("Error! Please enter the word in lowercase.\n");
-            return 1;
-        }
-    }
-
-    wordLength = strlen(word);
-
+    char curr[wordLength + 1];
     for (i = 0; i < wordLength; i++) {
         curr[i] = '_';
     }
     curr[wordLength] = '\0';
 
+    int hintGiven = 0;
+
     while (wrong < 6 && gameWon != 1) {
         printf("____\n");
         printf("|  |\n");
-        if (wrong >= 1) printf("|  0\n");
-        if (wrong == 2) printf("|  |\n");
-        if (wrong == 3) printf("| /|\n");
-        if (wrong >= 4) printf("| /|\\\n");
-        if (wrong == 5) printf("| / \n");
-        if (wrong == 6) printf("| / \\ \n");
-        printf("|\n");
+        if (wrong >= 1) printf("|  0 \n");
+        if (wrong == 2) printf("|  | \n");
+        if (wrong == 3) printf("| /| \n");
+        if (wrong >= 4) printf("| /|\\ \n");
+        if (wrong >= 5) printf("| / \\ \n");
+        printf("| \n");
 
         printf("Current word: %s\n", curr);
 
         char guess;
         printf("Enter a letter to guess or type '1' for a hint: ");
         scanf(" %c", &guess);
-        printf("\n--------------------------------------------------- \n");
-
-        if (!islower(guess) && guess != '1') {
-            printf("--------------------------------------------------- \n");
-            printf("Error! Please enter a single lowercase letter or 'H' for a hint.\n");
-            printf("--------------------------------------------------- \n");
-            continue;
-        }
 
         if (guess == '1') {
             if (hintGiven > 0) {
-                printf("--------------------------------------------------- \n");
                 printf("No more hints available.\n");
-                printf("--------------------------------------------------- \n");
+                continue;
             } else {
                 for (i = 0; i < wordLength; i++) {
                     if (curr[i] == '_') {
-                        curr[i] = word[i];
+                        curr[i] = word[r][i];
                         hintGiven = 1;
                         break;
                     }
                 }
+                continue;
             }
+        }
+
+        if (!islower(guess)) {
+            printf("Error! Please enter a single lowercase letter.\n");
             continue;
         }
 
         int alreadyGuessed = 0;
         for (i = 0; i < wordLength; i++) {
             if (curr[i] == guess) {
-                printf("--------------------------------------------------- \n");
                 printf("You already guessed '%c'. Try again.\n", guess);
-                printf("--------------------------------------------------- \n");
                 alreadyGuessed = 1;
                 break;
             }
@@ -89,16 +73,14 @@ int main() {
 
         int letterFound = 0;
         for (i = 0; i < wordLength; i++) {
-            if (word[i] == guess) {
+            if (word[r][i] == guess) {
                 curr[i] = guess;
                 letterFound = 1;
             }
         }
 
-        if (strcmp(curr, word) == 0) {
-            printf("--------------------------------------------------- \n");
-            printf("Congratulations! You guessed the right word: %s\n", word);
-            printf("--------------------------------------------------- \n");
+        if (strcmp(curr, word[r]) == 0) {
+            printf("Congratulations! You guessed the right word: %s\n", word[r]);
             gameWon = 1;
         }
 
@@ -107,11 +89,8 @@ int main() {
         }
     }
 
-
     if (wrong >= 6 && gameWon != 1) {
-        printf("--------------------------------------------------- \n");
-        printf("You die! The word was: %s\n", word);
-        printf("--------------------------------------------------- \n");
+        printf("You lose! The word was: %s\n", word[r]);
     }
 
     return 0;
